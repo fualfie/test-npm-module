263f35e2d64535fbfd1b8b71acb1120fe0e4f21c
Alfie Fu <44627885+fualfie@users.noreply.github.com>
2020-08-30 14:30:24 +0800

    add node module

--- index.js
+++ index.js
@@ -4,6 +4,9 @@ test.hello = () => {
     console.log('Hello World')
 }
 
+const uniqueString = require('unique-string');
+
 exports = module.exports = {};
 exports.test = test
 exports.my = require('./my')
+exports.random = ()=> uniqueString()
